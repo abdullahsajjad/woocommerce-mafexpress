4047c507cbcb7e597ce076f0ea1fb44e9e81ba4a
Abdullah <abdullahsajjad33@gmail.com>
2021-08-09 11:42:54 +0500

    code structure and documentation improved

--- inc/helpers.php
+++ inc/helpers.php
@@ -1,17 +1,22 @@
 <?php
+/**
+ * Plugin Helper Functions
+ *
+ * @since 2.0.0
+ */
+defined( 'ABSPATH' ) || die();
 
-defined('ABSPATH') || die();
-
-if( !function_exists( 'maf_request_log' ) ) {
+if ( ! function_exists( 'maf_request_log' ) ) {
 	/**
 	 * Logs every request sent to forms in logs.txt
 	 *
 	 * @param $request
 	 *
+	 * @since 2.0.0
 	 */
 	function maf_request_log( $request ) {
-		$file = plugin_dir_path( __FILE__ ).'/maf-logs.txt';
-		$eol = "\r\n";
+		$file     = MAFF_PATH . '/maf-logs.txt';
+		$eol      = "\r\n";
 		$contents = serialize( $request ) . $eol;
 		file_put_contents( $file, $contents, FILE_APPEND );
 	}
